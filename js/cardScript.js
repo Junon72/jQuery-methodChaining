abc79ea04d33c07362e5f2397d9f53202e3a2f4d
Jussi Nousiainen <juno.athome@gmail.com>
2019-10-15 12:11:31 +0200

    finished traversing Challenge - 1

--- js/cardScript.js
+++ js/cardScript.js
@@ -8,3 +8,20 @@ $('.stream').on('click', function () {
     $(thisStreamSelector).addClass('card-highlight');
     
     });
+
+$('.card_para').click(function() {
+    
+    // selector for the paragraph within individual card
+    var paraLinks = $(this).children('a');
+
+    $('a').removeClass('links-highlight');
+    $(paraLinks). addClass('links-highlight');
+
+    // another option without adding class to the css file - doesn't return the color back though
+    /**
+     * $('p').click(function() {
+     *      $(this).children('a').css('background-color', 'yellow');
+     * });
+     */
+
+});
